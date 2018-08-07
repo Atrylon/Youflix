52a29c9c6ab06b90b3b8460e5be8a36ef88965f1
Berenger Desgardin <berenger.desgardin@gmail.com>
2018-08-07 21:09:24 +0200

    correction bug addVideo

--- src/Event/VideoDeletedEvent.php
+++ src/Event/VideoDeletedEvent.php
@@ -14,7 +14,7 @@ use Symfony\Component\EventDispatcher\Event;
 
 class VideoDeletedEvent extends Event
 {
-    const NAME = "video.added";
+    const NAME = "video.deleted";
     protected $video;
 
     public function __construct(Video $video)
